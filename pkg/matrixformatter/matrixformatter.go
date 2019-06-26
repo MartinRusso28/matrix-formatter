5fde47ccb87032b20a92880504e6d44d832470da
Martin.Russo <martin_russo28@hotmail.com>
2019-06-26 00:31:46 -0300

    Adds flatten method

--- pkg/matrixformatter/matrixformatter.go
+++ pkg/matrixformatter/matrixformatter.go
@@ -3,10 +3,10 @@ package matrixformatter
 import (
 	"encoding/csv"
 	"fmt"
-	"strings"
 	"io"
-	"github.com/MartinRusso28/matrix-formatter/internal/network"
+	"strings"
 
+	"github.com/MartinRusso28/matrix-formatter/internal/network"
 )
 
 //Echo return the matrix as a string in matrix format.
@@ -19,7 +19,6 @@ func Echo(file io.Reader) network.Response {
 	if err != nil {
 		response.Error = err
 		response.StatusCode = 500
-		// w.Write([]byte(fmt.Sprintf("error %s", err.Error())))
 		return response
 	}
 	for _, row := range records {
@@ -54,8 +53,8 @@ func Invert(file io.Reader) network.Response {
 
 //Flatten return the matrix as a 1 line string, with values separated by commas.
 func Flatten(file io.Reader) network.Response {
-	var body string
-
+	var rows []string
+	
 	response := network.Response{}
 
 	records, err := csv.NewReader(file).ReadAll()
@@ -65,10 +64,10 @@ func Flatten(file io.Reader) network.Response {
 		return response
 	}
 	for _, row := range records {
-		body = fmt.Sprintf("%s %s", body, strings.Join(row, ","))
+		rows = append(rows, fmt.Sprintf("%s", strings.Join(row, ",")))
 	}
 
-	response.Body = body
+	response.Body = strings.Join(rows, ",")
 
 	return response
 }
