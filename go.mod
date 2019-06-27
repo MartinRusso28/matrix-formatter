26157c9385944f40c1e2a905cfe08e5cdd83fcc3
Martin.Russo <martin_russo28@hotmail.com>
2019-06-27 16:01:55 -0300

    remove unnecesary dependencies

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/MartinRusso28/matrixfmt
 go 1.12
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a
 	github.com/gin-gonic/gin v1.4.0
 	github.com/stretchr/testify v1.3.0
 )
--- internal/network/response.go
+++ internal/network/response.go
@@ -1,22 +1,11 @@
 package network
 
-import (
-	"github.com/asaskevich/govalidator"
-)
-
 //Response represent a request response.
 type Response struct {
 	StatusCode int `valid:"required"`
 	Body       string
 	Error      error
 }
-
-//Valid response struct.
-func (r Response) Valid() bool {
-	valid, _ := govalidator.ValidateStruct(r)
-	return valid
-}
-
 //InternalServerError response.
 func InternalServerError(err error) Response {
 	response := Response{
